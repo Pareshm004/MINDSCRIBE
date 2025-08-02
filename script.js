5b338bf05a946820358660534fbaea8cccbda2f8
Paresh Malviya <139699924+Pareshm004@users.noreply.github.com>
2025-08-03 00:37:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["your digital journal &#9996;....", "your mental wellness companion &#128524;....", "your guide &#129311;....", "an emotional mirror &#129303;....", "your FRIEND  &#129309;...."],
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 30,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
